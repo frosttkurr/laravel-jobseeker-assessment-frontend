3e16bb38bbcde150207bbea39806a4024bcb946b
frosttkurr <thisiscyber13@gmail.com>
2023-08-20 20:01:09 +0800

    change icon table

--- resources/views/candidates/edit.blade.php
+++ resources/views/candidates/edit.blade.php
@@ -33,31 +33,14 @@
     <div class="sidebar">
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-          </li>
           <li class="nav-item menu-open">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-table"></i>
               <p>
                 Tables
-                <i class="fas fa-angle-left right"></i>
+                <i class="fas fa-angle-down right"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="../tables/data.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>DataTables</p>
-                </a>
-              </li>
-            </ul>
           </li>
         </ul>
       </nav>
